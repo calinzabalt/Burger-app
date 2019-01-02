b6477afa11fa25af72277157f96a0b0693dfd2fa
calinzabalt <calinzabalt@gmail.com>
2019-01-02 12:25:30 +0200

    Updates

---
+++ precache-manifest.4409cfd7d4a98133d85e4ce8cbc921d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-app/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "4fcc1b81efbe04d59f9e",
+    "url": "/Burger-app/static/js/runtime~main.4fcc1b81.js"
+  },
+  {
+    "revision": "0972074c598529263e53",
+    "url": "/Burger-app/static/js/main.0972074c.chunk.js"
+  },
+  {
+    "revision": "54252f1a25fc928b3cc2",
+    "url": "/Burger-app/static/js/1.54252f1a.chunk.js"
+  },
+  {
+    "revision": "0972074c598529263e53",
+    "url": "/Burger-app/static/css/main.b2cac876.chunk.css"
+  },
+  {
+    "revision": "ab0f61571e4557c806d69d9b80eef72f",
+    "url": "/Burger-app/index.html"
+  }
+];
